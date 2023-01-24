68610159f0905414a5e54f8d8750b93999672cf3
larissamunhoz <larissa.munhoz.03@gmail.com>
2023-01-24 15:43:37 -0300

    commit inicial

---
+++ api-entrega-spring/src/main/java/com/algaworks/algalog/ApiEntregaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEntregaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEntregaSpringApplication.class, args);
+	}
+
+}
